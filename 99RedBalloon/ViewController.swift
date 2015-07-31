a6bbd48690e47f5fb001f731f855133654028d90
lapacino <lapacino@lapacinos-Mac-Pro.local>
2015-07-31 19:16:14 +0100

    completed

--- 99RedBalloon/ViewController.swift
+++ 99RedBalloon/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var balloonLabel: UILabel!
+    
+    var count = 0
+    var currentIndex = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.imageView.image = UIImage(named: "BerlinTVTower.jpg")
+        self.balloonLabel.text = "\(count) Balloon"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextBarButton(sender: UIBarButtonItem) {
+        
+        
+        var image1 = UIImage(named: "RedBalloon1.jpg")
+        var image2 = UIImage(named: "RedBalloon2.jpg")
+        var image3 = UIImage(named: "RedBalloon3.jpg")
+        var image4 = UIImage(named: "RedBalloon4.jpg")
+
+        var images = [image1, image2, image3, image4]
+        var indexPath = Int(arc4random_uniform(UInt32(images.count)))
+        while currentIndex == indexPath {
+            indexPath = Int(arc4random_uniform(UInt32(images.count)))
+        }
+        
+        currentIndex = indexPath
+
+        self.imageView.image = images[indexPath]
+        self.balloonLabel.text = "\(count++) Balloon"
+
+        
+    }
+    
 
 }
 
